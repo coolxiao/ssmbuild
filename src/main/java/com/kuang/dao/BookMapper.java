3c51a47c651f2fc8f8677267d635be9ad31c34fa
jiexiaoqiang <877023699@qq.com>
2021-04-05 22:48:25 +0800

    1

---
+++ src/main/java/com/kuang/dao/BookMapper.java
@@ -0,0 +1,18 @@
+package com.kuang.dao;
+
+import com.kuang.pojo.Books;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface BookMapper {
+    int addBook(Books books);
+
+    int deleteBookById(@Param("bookID") int id);
+
+    int updateBook(Books books);
+
+    Books queryBookById(@Param("bookID") int id);
+
+    List<Books> queryAllBook();
+}
